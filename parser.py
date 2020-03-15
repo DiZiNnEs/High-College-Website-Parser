d808c33fe8e27112ae793c7b7a211423c14f2c70
DiZiNnes <dizinens228@mail.ru>
2020-03-15 14:18:45 +0600

    Continue work!

--- parser.py
+++ parser.py
@@ -1,24 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from textwrap import dedent
 
 headers = {
-    'cookie': 'SOVASESSION_ID=6qg9lf2m1na0s958p6pqom51tu'
+    'cookie': 'SOVASESSION_ID=aoqf02h693q3502fivesag6li1'
 }
 
-page = 'http://vk-sova.3w.kz/users'
+page = f'http://vk-sova.3w.kz/users/'
 
 
 def parser_sova(headers, page):
     session = requests.Session()
     request = session.get(page, headers=headers)
     if request.status_code == 200:
-        #soup = BeautifulSoup(page, 'html.parser')
         soup = BeautifulSoup(request.content, 'html.parser')
-        a = soup.select('title')
-        print(a)
+        for users in soup.find_all('tr'):
+            name = users.select_one(':nth-child(3) a')
+            ip = users.select_one(':nth-child(4) span')
+            ip2 = users.select_one(':nth-child(5) span')
+            first_ip = ip['title'] if ip is not None else None
+            second_ip = ip2['title'] if ip2 is not None else None
+            print(dedent(f'''\
+            --------------------------------------------------
+                Full name : {name.contents}
+                IP from the last entry point: {first_ip}
+                IP from the place of registration: {second_ip}
+            --------------------------------------------------
+            '''))
+
     else:
         print('error')
 
-
+print(page)
 parser_sova(headers, page)
